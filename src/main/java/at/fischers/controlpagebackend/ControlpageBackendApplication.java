ec5447c0d5d8f4cf1871c8521748508d31465906
Simon Fischer <simi.fischa@gmail.com>
2020-08-30 15:11:13 +0200

    Initial commit

---
+++ src/main/java/at/fischers/controlpagebackend/ControlpageBackendApplication.java
@@ -0,0 +1,13 @@
+package at.fischers.controlpagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlpageBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlpageBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fischers/controlpagebackend/ControlpageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fischers.controlpagebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlpageBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
